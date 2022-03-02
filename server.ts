b9c60feb97ec7dcec1cbce720a15d22972cf7a99
Marck C. Guzmán <marck@marck-devs.com>
2022-03-02 22:34:47 +0100

    Feat: created the main server

---
+++ server.ts
@@ -0,0 +1,54 @@
+import {SimpleLogger, logger}  from "mk-simple-logger";
+import express from "express";
+import * as dotenv from "dotenv";
+import routes from "./routes";
+const app = express();
+
+// TODO: delete this
+process.env.ENVIROMENT = "dev";
+
+// ----[ ENV SETUP ]----
+switch (process.env.ENVIROMENT) {
+  case "prod":
+  case "production":
+    dotenv.config();
+    SimpleLogger.setLogLevel("warn");
+    break;
+  case "develop":
+  case "dev":
+  case "development":
+    dotenv.config({ path: __dirname + "/dev.env" });
+    SimpleLogger.setLogLevel("debug");
+    break;
+}
+
+// middlewares for general use 
+// ----[ PLUGINS ]----
+app.use(logger("SERVER", "info", "{method} {url} - {status} {time}ms"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+// logger config
+// ----[ LOG CONF ]----
+SimpleLogger.setDateFormat("[{day}-{month}-{year}] - [ {hour}:{min}:{sec} ]");
+SimpleLogger.setFormat("{date} - [ {level} ] - [ {name} ] - {msg}");
+SimpleLogger.global().setName("APP");
+
+// ----[ ROUTES ]----
+app.use(routes);
+app.use("/*", (res, rep)=>{
+  rep.send(`
+  <html>
+    <body>
+    <h1> Hi!</h1>
+    <p>Thanks for your data! 😘</p>
+    <small>Now I have your IP. Thanks (retard)</small>
+    </body>
+    </html>
+  `)
+})
+
+// ----[ SERVER INIT ]----
+let server = app.listen(process.env.PORT || 8080, ()=>{
+  SimpleLogger.global().info("Set up on port: {port}", {port: process.env.PORT || 8080});
+})
