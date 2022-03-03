dc74160b1496fa78a36683d1e6bf0f1609826d27
Marck C. Guzmán <marck@marck-devs.com>
2022-03-03 21:45:05 +0100

    Feat: created the get project endpoint

--- controllers/models/project.ts
+++ controllers/models/project.ts
@@ -5,7 +5,7 @@ import { errorLogger } from '../../helpers/error';
 import { ProjectDao } from '../../models';
 import { v4 } from 'uuid';
 
-interface ServerResponse{
+interface ServerResponse {
   status: string;
   code: number;
   message: string;
@@ -29,7 +29,7 @@ interface ServerResponse{
  * @param response {Response}
  */
 export async function createProyect(request: Request, response: Response) {
-  const LOG : SimpleLogger = new SimpleLogger("CRE_PROJECT")
+  const LOG: SimpleLogger = new SimpleLogger("CRE_PROJECT")
   const REQUEST_DATA = request.body;
   let sendResponse: ServerResponse = {
     status: "error",
@@ -38,24 +38,24 @@ export async function createProyect(request: Request, response: Response) {
   }
   let status: number = 400;
 
-  try{
-    if(!REQUEST_DATA.PRNAM){
+  try {
+    if (!REQUEST_DATA.PRNAM) {
       LOG.error("Name not found in request!");
       throw "Name not found"
     }
     REQUEST_DATA.PRID = v4();
     let project = ProjectDao.build(REQUEST_DATA);
-    LOG.debug("Data to save: {d}", {d: JSON.stringify(REQUEST_DATA)});
+    LOG.debug("Data to save: {d}", { d: JSON.stringify(REQUEST_DATA) });
     let pro = await project.save();
     LOG.info("Saved new project");
     status = 201;
     sendResponse.code = status;
     sendResponse.message = pro.toJSON();
     sendResponse.status = "OK";
-  }catch(err){
+  } catch (err) {
     errorLogger(LOG, err);
     sendResponse.code = status;
-    sendResponse.message = (String)(typeof err != "object" ? err: JSON.stringify(err));
+    sendResponse.message = (String)(typeof err != "object" ? err : JSON.stringify(err));
     sendResponse.status = "error";
   }
   response.status(status).json(sendResponse);
@@ -67,45 +67,47 @@ export async function createProyect(request: Request, response: Response) {
  * @param request {Request}
  * @param response {Response}
  */
-export async function deleteProject(request: Request, response: Response){
-  const LOG : SimpleLogger = new SimpleLogger("DEL_PROJECT");
-  const PRID : string = request.params.id;
-  let sendResp : ServerResponse = {
-    status : "error",
+export async function deleteProject(request: Request, response: Response) {
+  const LOG: SimpleLogger = new SimpleLogger("DEL_PROJECT");
+  const PRID: string = request.params.id;
+  let sendResp: ServerResponse = {
+    status: "error",
     message: "",
     code: 400
   }
   let status = 400;
-  try{
-    if(!PRID){
+  try {
+    if (!PRID) {
       LOG.warn("Project ID not found in request");
       throw "Project ID not found in request";
     }
-    let project = await ProjectDao.destroy({where:{
-      PRID
-    }});
+    let project = await ProjectDao.destroy({
+      where: {
+        PRID
+      }
+    });
     sendResp = {
       status: "ok",
       code: 202,
       message: "deleted"
     }
-    if(project == 0){
+    if (project == 0) {
       status = 404;
       LOG.warn("Project not found");
       throw "not found";
     }
     status = 202;
     sendResp = {
-      status : "ok",
+      status: "ok",
       message: "updated",
       code: status
     }
-    LOG.info("Deleted {PRID} Total: {project}", {PRID, project});
-  }catch(err){
-    errorLogger(LOG,err);
+    LOG.info("Deleted {PRID} Total: {project}", { PRID, project });
+  } catch (err) {
+    errorLogger(LOG, err);
     sendResp = {
-      status : "error",
-      message: (String)(typeof err != "object" ? err: JSON.stringify(err)),
+      status: "error",
+      message: (String)(typeof err != "object" ? err : JSON.stringify(err)),
       code: status
     }
   }
@@ -129,40 +131,46 @@ export async function deleteProject(request: Request, response: Response){
  * @param request {Request}
  * @param response {Response}
  */
-export async function updateProject(request: Request, response: Response){
-  const LOG:SimpleLogger = new SimpleLogger("UPDATE_PROJECT");
+export async function updateProject(request: Request, response: Response) {
+  const LOG: SimpleLogger = new SimpleLogger("UPDATE_PROJECT");
   const PRID = request.params.id;
   const DATA = request.body;
-  let sendResp : ServerResponse = {
+  let sendResp: ServerResponse = {
     code: 400,
     message: "",
     status: "error"
   };
   let status = 400;
-  try{
-    if(!PRID){
+  try {
+    if (!PRID) {
       LOG.critical("Project id not found in url");
       throw "Project id not found in url";
     }
-    if(!DATA){
+    if (!DATA) {
       LOG.warn("No body found");
       throw "No body found";
     }
-    let result : Array<number> = await ProjectDao.update(DATA, {
+    let result: Array<number> = await ProjectDao.update(DATA, {
       where: {
         PRID
       }
     });
-    if(result.length === 0){
+    if (result.length === 0) {
       status = 404;
       LOG.warn("Not found");
       throw "Project not found";
     }
-  }catch(error){
+    status = 202
+    sendResp = {
+      code: status,
+      message: "updated",
+      status: "ok"
+    }
+  } catch (error) {
     errorLogger(LOG, error);
     sendResp = {
       code: status,
-      message: (String)(typeof error != "object" ? error: JSON.stringify(error)),
+      message: (String)(typeof error != "object" ? error : JSON.stringify(error)),
       status: "error"
     }
   }
@@ -170,8 +178,48 @@ export async function updateProject(request: Request, response: Response){
 }
 
 
-export async function listProjects(request: Request, response: Response){
-  const LOG : SimpleLogger = new SimpleLogger("LS_PROJECT");
+export async function listProjects(request: Request, response: Response) {
+  const LOG: SimpleLogger = new SimpleLogger("LS_PROJECT");
+  LOG.info("Listing...")
   let data = await ProjectDao.findAll();
   response.status(200).json(data);
+}
+
+export async function getProject(request: Request, response: Response) {
+  const LOG : SimpleLogger = new SimpleLogger("GET_PROJECT");
+  const PRID = request.params.id;
+  let sendResp: ServerResponse = {
+    code: 400,
+    message: "",
+    status: "error"
+  };
+  let status: number = 400;
+  try{
+    if(!PRID){
+      LOG.error("No Project id found");
+      throw "No Project id found";
+    }
+    let data = await ProjectDao.findOne({
+      where: { PRID }
+    });
+    let msg = data?.toJSON();
+    if(!msg){
+      LOG.error("No Project found");
+      throw "Project not found";
+    }
+    status = 200;
+    sendResp = {
+      code: status,
+      message: msg,
+      status: "ok"
+    }
+  }catch(error){
+    errorLogger(LOG, error);
+    sendResp = {
+      code: status,
+      message: (String)(typeof error != "object" ? error : JSON.stringify(error)),
+      status: "error"
+    }
+  }
+  response.status(status).json(sendResp);
 }
--- routes/models/project.ts
+++ routes/models/project.ts
@@ -6,5 +6,6 @@ router.put("/", models.createProyect);
 router.delete("/:id", models.deleteProject);
 router.post("/:id", models.updateProject);
 router.get("/all", models.listProjects);
+router.get("/:id", models.getProject);
 
 export  {router as projectRouter};
