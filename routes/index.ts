b550b296bf6c7b2fc8bdf405fd62e34394f5a2c7
Marck C. Guzmán <marck@marck-devs.com>
2022-03-05 15:19:50 +0100

    Feat(routes): add 404 error message

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,17 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { ServerResponse } from "../controllers/models/project";
 import { dbtools } from "./dbtools";
 import { modelsRouter } from "./models";
 
 let router : Router = Router();
 router.use(dbtools);
 router.use(modelsRouter)
+router.use("*", (req: Request, res: Response)=>{
+  let response : ServerResponse = {
+    code: 404,
+    message: "not found " + req.baseUrl ,
+    status: "error"
+  }
+  res.status(404).json(response);
+})
 export default router;
